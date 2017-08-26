01912fc12b9853c2ce21ea033779e327fef7a00b
Corbin Walker <cdog10123@gmail.com>
2017-08-25 20:27:07 -0600

    fixed

---
+++ Scripting2670/Assets/Move.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Move : MonoBehaviour {
+
+	Action KeyAction;
+
+
+	// Use this for initialization
+	private void Start () {
+		KeyAction = Moves;
+	}
+	
+	void Moves() {
+		print("LeftArrow");
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			KeyAction();
+		}
+	}
+}
